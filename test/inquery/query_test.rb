16fd57cc6b8545393b582f5665cc6428e3fd0001
Remo Fritzsche <remo.fritzsche@sitrox.com>
2016-06-07 17:17:30 +0200

    Slight refactoring of test db creation.

--- test/inquery/query_test.rb
+++ test/inquery/query_test.rb
@@ -9,13 +9,7 @@ module Inquery
 
     def setup
       self.class.setup_db
-
-      Group.create!(name: 'Group 1')
-      Group.create!(name: 'Group 2')
-      Group.create!(name: 'Group 3')
-      User.create!(name: 'User 1', groups: Group.find([1, 2]))
-      User.create!(name: 'User 2', groups: Group.find([1, 3]))
-      User.create!(name: 'User 3', groups: Group.find([2]))
+      self.class.setup_base_data
     end
 
     def test_fetch_all_users
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,5 +12,15 @@ module TestHelper
     def setup_db
       load File.dirname(__FILE__) + '/db/schema.rb'
     end
+
+    def setup_base_data
+      Group.create!(name: 'Group 1')
+      Group.create!(name: 'Group 2')
+      Group.create!(name: 'Group 3')
+
+      User.create!(name: 'User 1', groups: Group.find([1, 2]))
+      User.create!(name: 'User 2', groups: Group.find([1, 3]))
+      User.create!(name: 'User 3', groups: Group.find([2]))
+    end
   end
 end
