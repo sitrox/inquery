aa0e960e825463b80eafd8bacfbd89037229956b
Jan Sandberg <jan.sandberg@sitrox.com>
2019-10-09 15:22:58 +0200

    RawSqlUtils: Add method for SQL sanitization
    The instance method `san` is made available for all Inquery::Query
    objects.

--- lib/inquery/mixins/raw_sql_utils.rb
+++ lib/inquery/mixins/raw_sql_utils.rb
@@ -4,6 +4,11 @@ module Inquery
       extend ActiveSupport::Concern
 
       included do
+        # Sanitizes the SQL and substitutes in the supplied variables. Relies on
+        # `sanitize_sql_array` from ActiveRecord.
+        def san(sql, *variables)
+          ActiveRecord::Base.send(:sanitize_sql_array, [sql, *variables])
+        end
       end
     end
   end
